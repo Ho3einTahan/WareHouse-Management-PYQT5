7221a422d6128666fae566edb57ff1a5fb7b37f7
ho3eintahan <hoseintahan84@gmail.com>
2025-05-31 14:31:20 +0330

    Add      product_repository.py

---
+++ Repository/product_repository.py
@@ -0,0 +1,17 @@
+from DataSource.product_datasource import ProductDataSource
+from Model.product import Product
+
+
+class ProductRepository:
+
+    def __init__(self, productDataSource: ProductDataSource):
+        self.productDataSource = productDataSource
+
+    def addNewProduct(self, product: Product):
+        self.productDataSource.addNewProduct(product)
+
+    def getAllProducts(self):
+        return self.productDataSource.getAllProducts()
+
+    def updateProduct(self, product: Product):
+        self.productDataSource.updateProductById(product)
