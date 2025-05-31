519deb594089bd888476b7fd3665c7b7b1aa7768
ho3eintahan <hoseintahan84@gmail.com>
2025-05-31 14:30:20 +0330

    Update     product.py

--- model/product.py
+++ model/product.py
@@ -1,6 +1,18 @@
 class Product:
-    def __init__(self, prCode, prName, buyPrice, sellPrice, inventory, desc):
-        self.productCode = prCode
+    def __init__(
+        self,
+        prCode=None,
+        prName=None,
+        buyPrice=None,
+        sellPrice=None,
+        inventory=None,
+        desc=None,
+    ):
+        super().__init__()
+        if prCode is not None:
+            self.productCode = prCode
+        else:
+            self.productCode = None
         self.productName = prName
         self.buyPrice = buyPrice
         self.sellPrice = sellPrice
